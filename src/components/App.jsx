0c95a5d123d70919546c370bb2ffa7580d98df4f
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-07-15 15:15:12 +0200

    added componentDidMount and componentDidUpdate to load to/from local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,31 @@ import Form from './Form/Form';
 import ContactList from './ContactList/ContactList';
 
 class App extends Component {
-  // state = {
-  //   contacts: [],
-  //   filter: '',
-  // };
-
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
   };
+
+  // state = {
+  //   contacts: [
+  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ],
+  //   filter: '',
+  //   name: '',
+  //   number: '',
+  // };
 
   addContact = newContact => {
     const { contacts } = this.state;
 
     const doesContactExist = contacts.some(
       contact =>
-        contact.id === newContact.is || contact.name === newContact.name
+        contact.id === newContact.id || contact.name === newContact.name
     );
 
     if (doesContactExist) {
@@ -42,6 +44,22 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
+
+  // componentDidMount() {
+  //   // ta metoda na starcie załaduje do ls podane w state kontakty
+  //   localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  // }    
+
+   componentDidMount() {
+    const storedContacts = 
+      localStorage.getItem('contacts');
+      this.setState({contacts: JSON.parse(storedContacts)})
+  }
+
+  componentDidUpdate() {
+    // ta metoda działa zawsze jak aktualizujemy state lub dostajemy props z góry
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   render() {
     return (
